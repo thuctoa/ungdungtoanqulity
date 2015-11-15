0c76ddae6a4f6d734735dea9da387de01cd5c7fc
Nguyen The Thuc <thucfami@gmail.com>
2015-11-15 07:00:13 +0700

    Môi trường đảm bảo chất lượng

---
+++ models/Book.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%book}}".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property integer $user_id
+ * @property string $isbn
+ *
+ * @property User $user
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%book}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'description'], 'required'], 
+            [['user_id'], 'integer'],
+            [['title'], 'string', 'max' => 64],
+            [['description'], 'string', 'max' => 1024],
+            [['isbn'], 'string', 'max' => 32],
+            [['user_id'], 'exist', 'targetClass'=>'\app\models\User', 'targetAttribute'=>'id', 'message'=>Yii::t('app','This user doesn\'t exist')],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+            'description' => Yii::t('app', 'Description'),
+            'user_id' => Yii::t('app', 'User'),
+            'isbn' => Yii::t('app', 'Isbn'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+}
